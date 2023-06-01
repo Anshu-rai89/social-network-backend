c8cb7641d84d54083fddfbdd1ebc0909970b6fe1
Anshu Rai <raianshu8991@gmail.com>
2023-06-02 00:23:08 +0530

    Added tweet distribution on follow

---
+++ functions/distribute-tweet.js
@@ -0,0 +1,109 @@
+const DynamoDB = require("@aws-sdk/client-dynamodb");
+const { unmarshall, marshall } = require("@aws-sdk/util-dynamodb");
+const {
+  DynamoDBDocumentClient,
+  QueryCommand,
+  BatchWriteCommand
+} = require("@aws-sdk/lib-dynamodb");
+const client = new DynamoDB.DynamoDBClient({});
+const docClient = DynamoDBDocumentClient.from(client);
+const { RELATIONSHIP_TABLE, TIMELINES_TABLE } = process.env;
+const Constants = require('../lib/constants');
+const _ = require('lodash');
+
+module.exports.handler = async (event) => {
+    for(const record of event.Records) {
+        if(record.eventName === 'INSERT') {
+            const tweet = unmarshall(event.dynamodb.NewImage);
+            const followers = await getFollowers(tweet.creator);
+            await distributeTweet(tweet, followers);
+        }else if(record.eventName === 'REMOVE') {
+            const tweet = unmarshall(event.dynamodb.NewImage);
+            const followers = await getFollowers(tweet.creator);
+            await unDistributeTweet(tweet, followers);
+        }
+    }
+};
+
+async function getFollowers(userId) {
+    const loop = async(acc, exclusiveStartKey)=> {
+        const query = new QueryCommand({
+          TableName: RELATIONSHIP_TABLE,
+          KeyConditions:
+            "otherUserId = :otherUserId and begins_with(sk, :follow)",
+          ExpressionAttributeValues: {
+            otherUserId: userId,
+            ":follow": "FOLLOWS_",
+          },
+          IndexName: "byOtherUser",
+          ExclusiveStartKey: exclusiveStartKey,
+        });
+
+        const resp = await docClient.send(query);
+        const userIds = (resp.Items || []).map(user=> user.id);
+
+        if(resp.LastEvaluatedKey) {
+            return await loop(acc.concat(userIds), resp.LastEvaluatedKey);
+        }
+        else {
+            return acc.concat(userIds);
+        }
+    }
+
+    return await loop([]);
+}
+
+async function distributeTweet(tweet, followers) {
+    const timelineEntries = followers.map((userId)=> {
+        PutRequest: {
+            Item: marshall({
+              userId,
+              tweetId: tweet.id,
+              timestamp: tweet.createdAt,
+              retweetOf: tweet.retweetOf,
+              distributedFrom: tweet.creator,
+              inReplyToTweetId: tweet.inReplyToTweetId,
+              inReplyToUserIds: tweet.inReplyToUserIds,
+            });
+        }
+    });
+
+    const chunks = _.chunk(timelineEntries, Constants.dynamodb.MAX_BATCH_SIZE);
+    const promises = chunks.map(async (chunk)=> {
+        const query = new BatchWriteCommand({
+          RequestItems: {
+            [TIMELINES_TABLE]: chunk
+          }
+        });
+        await docClient.send(query);
+    });
+
+    await Promise.all(promises)
+}
+
+async function unDistributeTweet(tweet, followers) {
+  const timelineEntries = followers.map((userId) => {
+    DeleteRequest: {
+      Item: marshall({
+        userId,
+        tweetId: tweet.id,
+        timestamp: tweet.createdAt,
+        retweetOf: tweet.retweetOf,
+        inReplyToTweetId: tweet.inReplyToTweetId,
+        inReplyToUserIds: tweet.inReplyToUserIds,
+      });
+    }
+  });
+
+  const chunks = _.chunk(timelineEntries, Constants.dynamodb.MAX_BATCH_SIZE);
+  const promises = chunks.map(async (chunk) => {
+    const query = new BatchWriteCommand({
+      RequestItems: {
+        [TIMELINES_TABLE]: chunk,
+      },
+    });
+    await docClient.send(query);
+  });
+
+  await Promise.all(promises);
+}
---
+++ functions/functions/distribute-tweet-to-follower.js
@@ -0,0 +1,141 @@
+const DynamoDB = require("@aws-sdk/client-dynamodb");
+const { unmarshall, marshall } = require("@aws-sdk/util-dynamodb");
+const {
+  DynamoDBDocumentClient,
+  QueryCommand,
+  BatchWriteCommand,
+} = require("@aws-sdk/lib-dynamodb");
+const client = new DynamoDB.DynamoDBClient({});
+const docClient = DynamoDBDocumentClient.from(client);
+const { TWEETS_TABLE, TIMELINES_TABLE ,MAX_TWEETS_SIZE} = process.env;
+const Constants = require("../lib/constants");
+const _ = require("lodash");
+const MaxTweetsSize = parseInt(MAX_TWEETS_SIZE);
+
+module.exports.handler = async (event) => {
+  for (const record of event.Records) {
+    if (record.eventName === "INSERT") {
+      const relationship = unmarshall(event.dynamodb.NewImage);
+      const [relType] = relationship.sk.split("_");
+      if (relType === "FOLLOW") {
+        const tweets = await getTweets(relationship.otherUserId);
+        await distributeTweet(tweets, relationship.otherUserId);
+      }
+     
+    } else if (record.eventName === "REMOVE") {
+     const relationship = unmarshall(event.dynamodb.NewImage);
+     const [relType] = relationship.sk.split("_");
+     if (relType === "FOLLOW") {
+       const tweets = await getTimelineEntries(relationship.otherUserId,relationship.userId);
+       await unDistributeTweet(tweets, relationship.otherUserId);
+     }
+    }
+  }
+};
+
+async function getTweets(userId) {
+  const loop = async (acc, exclusiveStartKey) => {
+    const query = new QueryCommand({
+      TableName: TWEETS_TABLE,
+      KeyConditions: "creator = :userId",
+      ExpressionAttributeValues: {
+        "userId": userId
+      },
+      IndexName: "byCreator",
+      ExclusiveStartKey: exclusiveStartKey,
+    });
+
+    const resp = await docClient.send(query);
+    const tweets = resp.Items;
+    const newAcc = acc.concat(tweets);
+
+    if (resp.LastEvaluatedKey && newAcc.length < MaxTweetsSize) {
+      return await loop(newAcc, resp.LastEvaluatedKey);
+    } else {
+      return newAcc;
+    }
+  };
+
+  return await loop([]);
+}
+
+async function getTimelineEntries(distributedFrom, userId) {
+  const loop = async (acc, exclusiveStartKey) => {
+    const query = new QueryCommand({
+      TableName: TIMELINES_TABLE,
+      KeyConditions: "distributedFrom = :distributedFrom and userId = :userId",
+      ExpressionAttributeValues: {
+        userId: userId,
+        distributedFrom: distributedFrom,
+      },
+      IndexName: "byDistributedFrom",
+      ExclusiveStartKey: exclusiveStartKey,
+    });
+
+    const resp = await docClient.send(query);
+    const tweets = resp.Items;
+    const newAcc = acc.concat(tweets);
+
+    if (resp.LastEvaluatedKey) {
+      return await loop(newAcc, resp.LastEvaluatedKey);
+    } else {
+      return newAcc;
+    }
+  };
+
+  return await loop([]);
+}
+async function distributeTweet(tweets, userId) {
+  const timelineEntries = tweets.map((tweet) => {
+    PutRequest: {
+      Item: marshall({
+        userId,
+        tweetId: tweet.id,
+        timestamp: tweet.createdAt,
+        retweetOf: tweet.retweetOf,
+        distributedFrom: tweet.creator,
+        inReplyToTweetId: tweet.inReplyToTweetId,
+        inReplyToUserIds: tweet.inReplyToUserIds,
+      });
+    }
+  });
+
+  const chunks = _.chunk(timelineEntries, Constants.dynamodb.MAX_BATCH_SIZE);
+  const promises = chunks.map(async (chunk) => {
+    const query = new BatchWriteCommand({
+      RequestItems: {
+        [TIMELINES_TABLE]: chunk,
+      },
+    });
+    await docClient.send(query);
+  });
+
+  await Promise.all(promises);
+}
+
+async function unDistributeTweet(tweets, userId) {
+  const timelineEntries = tweets.map((tweet) => {
+    DeleteRequest: {
+      Item: marshall({
+        userId,
+        tweetId: tweet.id,
+        timestamp: tweet.createdAt,
+        retweetOf: tweet.retweetOf,
+        inReplyToTweetId: tweet.inReplyToTweetId,
+        inReplyToUserIds: tweet.inReplyToUserIds,
+      });
+    }
+  });
+
+  const chunks = _.chunk(timelineEntries, Constants.dynamodb.MAX_BATCH_SIZE);
+  const promises = chunks.map(async (chunk) => {
+    const query = new BatchWriteCommand({
+      RequestItems: {
+        [TIMELINES_TABLE]: chunk,
+      },
+    });
+    await docClient.send(query);
+  });
+
+  await Promise.all(promises);
+}
