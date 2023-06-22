e5b8e08f7a7a207909a578789fe1aee45f969c5d
Anshu Rai <raianshu8991@gmail.com>
2023-06-22 17:23:28 +0530

    Fixed ssm integration issue

--- functions/sync-tweet-to-algolia.js
+++ functions/sync-tweet-to-algolia.js
@@ -7,38 +7,45 @@ const ssm = require('@middy/ssm');
 const { STAGE } = process.env;
 
 module.exports.handler = middy(async (event, context) => {
+  console.log("Context", context);
+  console.log("Stage", STAGE);
   const index = await initTweetIndex(
     context.ALGOLIA_APP_ID,
     context.ALGOLIA_WRITE_KEY,
     STAGE
   );
+
   for (const record of event.Records) {
     if (record.eventName === "INSERT" || record.eventName === "MODIFY") {
       const tweet = unmarshall(record.dynamodb.NewImage);
 
-      if (tweet.__typeName === TweetTypes.RETWEET) {
+      if (tweet.__typename === TweetTypes.RETWEET) {
         continue;
       }
 
       tweet.objectID = tweet.id;
+
       await index.saveObjects([tweet]);
     } else if (record.eventName === "REMOVE") {
       const tweet = unmarshall(record.dynamodb.OldImage);
-      if (tweet.__typeName === TweetTypes.RETWEET) {
+
+      if (tweet.__typename === TweetTypes.RETWEET) {
         continue;
       }
+
       await index.deleteObjects([tweet.id]);
     }
   }
 }).use(
   ssm({
     cache: true,
-    cacheExpiryInMillis: 5 * 60 * 1000,
-    names: {
+    cacheExpiryInMillis: 5 * 60 * 1000, // 5 mins
+    fetchData: {
       ALGOLIA_APP_ID: `/${STAGE}/aloglia-app-id`,
       ALGOLIA_WRITE_KEY: `/${STAGE}/algolia-admin-key`,
     },
     setToContext: true,
-    throwOnFailedCall: true
-  })
-);
+    throwOnFailedCall: true,
+  })).before((request) => {
+    globalDefaults = request.context?.defaults?.global;
+  });
--- functions/sync-user-to-algolia.js
+++ functions/sync-user-to-algolia.js
@@ -4,32 +4,39 @@ const {initUserIndex} = require('../lib/algolia');
 const middy = require("@middy/core");
 const ssm = require("@middy/ssm");
 const { STAGE } = process.env;
+console.log("STage", STAGE);
 
 module.exports.handler = middy(async (event, context) => {
-    const index = await initUserIndex(
-      context.ALGOLIA_APP_ID,
-      context.ALGOLIA_WRITE_KEY,
-      STAGE
-    );
-    for (const record of event.Records) {
+  console.log("Context", context);
+  const index = await initUserIndex(
+    context.ALGOLIA_APP_ID,
+    context.ALGOLIA_WRITE_KEY,
+    STAGE
+  );
+
+  for (const record of event.Records) {
     if (record.eventName === "INSERT" || record.eventName === "MODIFY") {
-        const profile = unmarshall(record.dynamodb.NewImage);
-        profile.objectID = profile.id;
-        await index.saveObjects([profile]);
+      const profile = unmarshall(record.dynamodb.NewImage);
+
+      profile.objectID = profile.id;
+
+      await index.saveObjects([profile]);
     } else if (record.eventName === "REMOVE") {
-        const profile = unmarshall(record.dynamodb.OldImage);
-        await index.deleteObjects([profile.id]);
+      const profile = unmarshall(record.dynamodb.OldImage);
+
+      await index.deleteObjects([profile.id]);
     }
-    }
+  }
 }).use(
   ssm({
     cache: true,
-    cacheExpiryInMillis: 5 * 60 * 1000,
-    names: {
+    cacheExpiryInMillis: 5 * 60 * 1000, // 5 mins
+    fetchData: {
       ALGOLIA_APP_ID: `/${STAGE}/aloglia-app-id`,
       ALGOLIA_WRITE_KEY: `/${STAGE}/algolia-admin-key`,
     },
     setToContext: true,
-    throwOnFailedCall: true
-  })
-);
+    throwOnFailedCall: true,
+  })).before((request) => {
+    globalDefaults = request.context?.defaults?.global;
+  });
